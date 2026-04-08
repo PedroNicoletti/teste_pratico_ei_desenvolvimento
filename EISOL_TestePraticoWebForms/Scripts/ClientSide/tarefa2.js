0697b5b270cdf37f6b869a66883e29a7a3ff3173
Pedro Nicoletti Sotoma <pedro@fazenda.ms.gov.br>
2026-04-07 20:26:31 -0400

    feat: Adicionando código da Tarefa 2

--- EISOL_TestePraticoWebForms/Scripts/ClientSide/tarefa2.js
+++ EISOL_TestePraticoWebForms/Scripts/ClientSide/tarefa2.js
@@ -1,8 +1,5 @@
-﻿/// <reference path="../../Scripts/jquery-1.10.2.js" />
+/// <reference path="../../Scripts/jquery-1.10.2.js" />
 /// <reference path="../../Scripts/bootstrap.js" />
-
-// Legal né, essas linhas mágicas aí de cima permitem o Visual Studio a montar o Intellisense do jQuery aqui como ele já faz com o C#.
-// Não apague elas, elas são do bem.
 
 "use strict"
 
@@ -11,6 +8,12 @@ var TAREFA2 = TAREFA2 || {
         $("[id$='_btnEstranho']").on('click', () => {
             return TAREFA2.Autodestruir();
         });
+
+        $("[id$='_btnGravar']").on('click', () => {
+            return TAREFA2.Validar();
+        });
+
+        TAREFA2.AplicarMascaras();
     },
     Autodestruir: () => {
         window.alert('Este computador se autodestruirá em 20 segundos...\r\nTodos os seus códigos serão descartados e não poderão ser recuperados.');
@@ -19,14 +22,64 @@ var TAREFA2 = TAREFA2 || {
         }, 3000);
         return false;
     },
-    Validar: ()=> {
-        // hummmm...
+    AplicarMascaras: () => {
+        $("[id$='_txtCpf']").on('input', function () {
+            var v = $(this).val().replace(/\D/g, '').substring(0, 11);
+            v = v.replace(/(\d{3})(\d)/, '$1.$2');
+            v = v.replace(/(\d{3})(\d)/, '$1.$2');
+            v = v.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+            $(this).val(v);
+        });
+
+        $("[id$='_txtTelefone']").on('input', function () {
+            var v = $(this).val().replace(/\D/g, '').substring(0, 11);
+            v = v.replace(/^(\d{2})(\d)/g, '($1) $2');
+            v = v.replace(/(\d)(\d{4})$/, '$1-$2');
+            $(this).val(v);
+        });
+
+        $("[id$='_txtDataNascimento']").on('input', function () {
+            var v = $(this).val().replace(/\D/g, '').substring(0, 8);
+            v = v.replace(/(\d{2})(\d)/, '$1/$2');
+            v = v.replace(/(\d{2})(\d)/, '$1/$2');
+            $(this).val(v);
+        });
+    },
+    ValidarEmail: (email) => {
+        var regex = /^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$/;
+        return regex.test(email);
+    },
+    Validar: () => {
+        var erros = [];
+
+        if ($.trim($("[id$='_txtNome']").val()) === '')
+            erros.push('Nome');
+
+        if ($("[id$='_txtCpf']").val().replace(/\D/g, '').length !== 11)
+            erros.push('CPF');
+
+        if ($.trim($("[id$='_txtRg']").val()) === '')
+            erros.push('RG');
+
+        var email = $.trim($("[id$='_txtEmail']").val());
+        if (email !== '' && !TAREFA2.ValidarEmail(email))
+            erros.push('Email');
+
+        if ($("[id$='_ddlSexo']").prop('selectedIndex') === 0)
+            erros.push('Sexo');
+
+        var dataNascimento = $.trim($("[id$='_txtDataNascimento']").val());
+        if (dataNascimento === '' || !/^\d{2}\/\d{2}\/\d{4}$/.test(dataNascimento))
+            erros.push('Data de nascimento');
+
+        if (erros.length > 0) {
+            window.alert('Verifique o(s) campo(s): ' + erros.join(', '));
+            return false;
+        }
+
+        return true;
     }
 }
-
-// Isso aqui são coisas que usamos pra fazer os scripts funcionarem bem com o WebForms
-// Pode ser que você tenha um código muito melhor!
-// Use este modelo se preferir.
 
 var postBackPage = postBackPage || Sys.WebForms.PageRequestManager.getInstance();
 
--- EISOL_TestePraticoWebForms/Tarefa2.aspx.cs
+++ EISOL_TestePraticoWebForms/Tarefa2.aspx.cs
@@ -1,82 +1,89 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace EISOL_TestePraticoWebForms
 {
-    public partial class Tarefa2 : System.Web.UI.Page
-    {
-        /*
-         * 
-         * 
-         * 
-         *  Sério mesmo que você acho que eu responderia a tarefa 1 aqui!?!?
-         *  Nananinanão.
-         *  Se ele funcionou lá, com certeza funcionará aqui! ;D
-         * 
-         * 
-         * 
-         * 
-         * */
+	public partial class Tarefa2 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+		}
 
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            // Para saber se o seu registro foi realmente adicionado à tabela, utilize um dos métodos de BLL.PESSOAS.
-            // Você poderá realizar a depuração aqui no VS e conferir se tudo deu certo.
-            // Sinta-se livre para fazer a sua arte, mas tente fazer o formulário funcionar ok!
-        }
+		protected void btnGravar_Click(object sender, EventArgs e)
+		{
+			var erros = new List<string>();
 
-        protected void btnGravar_Click(object sender, EventArgs e)
-        {
-            /* Olá!
-             * Trabalhamos com camadas de acesso a dados e negócios, isso também é conhecido por arquitetura em camadas ou N-Tier.
-             * Observe que passamos um objeto tipado da camada de acesso (DAO - Data Access Object).
-             * E devemos utilizar esse objeto DAO e chamar os métodos da camada de negócios (BLL - Business Logical Layer).
-             * É o que por padrão o MVC te induz a fazer, mas aqui no WebForms devemos ter esse cuidado para não dificultar as coisas criando códigos macarrônicos (eita).
-             * Você está livre para espiar os códigos e entender o seu funcionamento.
-             * Só não vai me bagunçar os códigos pois deu muito trabalho fazer tudo isso aqui =/
-             * */
+			if (string.IsNullOrWhiteSpace(txtNome.Text))
+				erros.Add("Nome");
 
-            var pessoa = new DAO.PESSOAS();
+			string cpf = Regex.Replace(txtCpf.Text, @"\D", "");
+			if (string.IsNullOrWhiteSpace(cpf))
+				erros.Add("CPF");
 
-			// Parece que faltam algumas coisas aqui! =/
+			if (string.IsNullOrWhiteSpace(txtRg.Text))
+				erros.Add("RG");
 
-			// O Objeto pessoa não parece ser uma pessoa de verdade ainda. 
-			// As pessoas não são objetos mas aqui podemos considerá-las assim =S
-			// - Faça as devidas atribuições ao objeto 'pessoa' para que ela seja uma pessoa de verdade e feliz!
+			if (ddlSexo.SelectedIndex == 0)
+				erros.Add("Sexo");
 
-			// Verifique os tamanhos dos campos da tabela e a obrigatoriedade deles e faça o devido tratamento para evitar erros.
-			// - O leiaute da tabela em questão (TB_TESTE_PESSOAS) poderá ser verificado nos arquivos .sql anexados ao projeto.
+			DateTime dataNascimento;
+			bool dataValida = DateTime.TryParseExact(txtDataNascimento.Text.Trim(), "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dataNascimento);
 
-			// Coloque o seu lindo código aqui! (O_o)
+			if (!dataValida)
+				erros.Add("Data de nascimento");
+
+			if (erros.Count > 0)
+			{
+				return;
+			}
+
+			string telefone = Regex.Replace(txtTelefone.Text, @"\D", "");
+
+			var pessoa = new DAO.PESSOAS();
+			pessoa.NOME = txtNome.Text.Trim();
+			pessoa.CPF = cpf;
+			pessoa.RG = txtRg.Text.Trim();
+			pessoa.TELEFONE = telefone;
+			pessoa.EMAIL = txtEmail.Text.Trim();
+			pessoa.SEXO = ddlSexo.SelectedValue;
+			pessoa.DATA_NASCIMENTO = dataNascimento;
 
 			this.Gravar(pessoa);
-        }
+			this.Limpar();
+		}
 
-        /// <summary>
-        /// Persistir os dados no Banco.
-        /// </summary>
-        /// <param name="pessoa">DAO.PESSOAS</param>
-        private void Gravar(DAO.PESSOAS pessoa)
-        {
-            // Se a pessoa for uma pessoa de verdade e feliz, com certeza ela será lembrada pelo banco de dados.
-            new BLL.PESSOAS().Adicionar(pessoa);
-            this.Alertar();
-        }
+		/// <summary>
+		/// Persistir os dados no Banco.
+		/// </summary>
+		/// <param name="pessoa">DAO.PESSOAS</param>
+		private void Gravar(DAO.PESSOAS pessoa)
+		{
+			new BLL.PESSOAS().Adicionar(pessoa);
+			this.Alertar();
+		}
 
-        /// <summary>
-        /// Apresentar o alerta de sucesso na operação.
-        /// </summary>
-        private void Alertar()
-        {
-            this.divAlerta.Visible = true;
-        }
+		/// <summary>
+		/// Apresentar o alerta de sucesso na operação.
+		/// </summary>
+		private void Alertar()
+		{
+			this.divAlerta.Visible = true;
+		}
 
-        /// <summary>
-        /// Limpar os campos após a presistência dos dados.
-        /// </summary>
-        private void Limpar()
-        {
-            // Isso é apenas um bônus!
-            // Tente fazê-lo e colocar em um lugar apropriado no código.
-        }
-    }
+		/// <summary>
+		/// Limpar os campos após a presistência dos dados.
+		/// </summary>
+		private void Limpar()
+		{
+			txtNome.Text = string.Empty;
+			txtCpf.Text = string.Empty;
+			txtRg.Text = string.Empty;
+			txtTelefone.Text = string.Empty;
+			txtEmail.Text = string.Empty;
+			ddlSexo.SelectedIndex = 0;
+			txtDataNascimento.Text = string.Empty;
+		}
+	}
 }
